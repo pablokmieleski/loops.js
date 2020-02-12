abd2aaaa0e4410d11ea8e9ec31ae3e5a47210591
pablokmieleski <pablo.kmieleski@gmail.com>
2020-02-12 19:53:56 +0000

    js funn

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log('hi');
+// console.log('hi');
+
+
+// for (let i = 0; i < 100; i++) {		
+//     const a = 'test';
+//     if (a.length === i){
+//     console.log(`${i+1} powtorzenie`);		
+//     }		
+// }
+
+// let a = 1;
+// console.log(a++);
+// console.log(a--);
+// console.log(a--);
+// console.log(a);
+
+// 1
+// 2
+// 1
+// 0
+
+
+
+// let b = 2;
+// console.log(++b);
+// console.log(--b);
+// console.log(b);
+
+// 3
+// 2
+// 2
+
+
+// const elements = ['x', 'y', 'z'];		
+// for (let i = 0; i < elements.length; i++) {		
+// console.log(`na miejscu ${i} jest ${elements[i]}`);		
+// }		
+
+// na miejscu 0 jest x
+// na miejscu 1 jest y
+// na miejscu 2 jest z
+
+
+const faces = ['smile', 'cry', 'sad', 'ble']
+
+for (let i = 0; i < faces.length; i++) {		
+    console.log(`i = ${i+1}`);	
+    console.log(`element ${i+1}: ${faces[i]}`);		
+    }		
+
